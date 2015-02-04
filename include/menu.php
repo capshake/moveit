b39eb82e525a4445a684ac290c9f4030121d90bd
Dane Podzkiewitz <dane.podzkiewitz+git@gmail.com>
2015-02-04 15:48:34 +0100

    started working on html for planner

--- include/menu.php
+++ include/menu.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="<?php echo BASEDIR; ?>">moveit</a>
+            <a class="navbar-brand" href="<?php echo BASEDIR; ?>">moveIT</a>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (!$userData->isLoggedIn()) {
     header('location: ' . BASEDIR . 'login');
 } else {
 
-    $headerTitle = 'dasdsad';
+    $headerTitle = 'moveIT';
 
 
     include_once 'include/header.php';
@@ -18,26 +18,98 @@ if (!$userData->isLoggedIn()) {
 
 
     <div class="container">
-
         <div class="row">
-            <div class="col-md-12">
-                <h1>MoveIT</h1>
-                <p class="lead">Startseite von unserem kleinen Projekt.</p>
-
-                <?php
-                if (isset($_POST['hi']) && $userData->isValidToken(@$_POST['token'])) {
-
-                    echo $_POST['hi'];
-
-
-                    $userData->newToken();
-                }
-                ?>
-                <form method="POST" action="<?php echo BASEDIR . 'index.php'; ?>">
-                    <input name="hi" value="ok">
-                    <input name="submit" type="submit" />
-                </form>
+            <!-- Altbau -->
+            <div class="col-md-3 altbau">
+                <div class="row">
+                    <h3>Altbau</h3>
+                </div>
+            
+                <div class="row">
+                    Trakt/Gebäude:<br>
+                    <select class="trakt">
+                        <option value="1">H-Trakt</option>
+                    </select>
+                </div>
+                <div class="row">
+                    Etage:<br>
+                    <select class="etage">
+                        <option value="-2">2. UG</option>
+                        <option value="-1">1. UG</option>
+                    </select>
+                </div>
+                <div class="row">
+                    Raum:<br>
+                    <select class="raum">
+                        <option value="1">H1.11</option>
+                    </select>
+                </div>
+                
+                <div class="row">
+                    <ul class="altbau liste">
+                        <li>Tisch</li>
+                        <li>Stuhl</li>
+                    </ul>
+                </div>
             </div>
+
+            <!-- Neubau -->
+            <div class="col-md-6 neubau">
+                <div class="row">
+                    <h3>Neubau</h3>
+                </div>
+
+                <div class="row">
+                    Trakt/Gebäude:
+                    <select class="trakt">
+                        <option value="1">H-Trakt</option>
+                    </select>
+
+                    Etage:
+                    <select class="etage">
+                        <option value="-2">2. UG</option>
+                        <option value="-1">1. UG</option>
+                    </select>
+
+                    Raum:
+                    <select class="raum">
+                        <option value="1">H1.11</option>
+                    </select>
+                </div>
+
+                <div class="row">
+                    Karte
+                </div>
+            </div>
+
+            <!-- Persönlich -->
+            <div class="col-md-3 persoenlich">
+                <div class="row">
+                    <div class="row">
+                        <h3>Lager, Wunschliste, Müll</h3>
+                    </div>
+                    <div class="row">
+                        <ul class="altbau liste">
+                            <li>Tisch</li>
+                            <li>Stuhl</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="row">
+                        <h3>Öffentliches Lager</h3>
+                    </div>
+                    <div class="row">
+                        <ul class="altbau liste">
+                            <li>Tisch</li>
+                            <li>Stuhl</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
 
     </div><!-- /.container -->
