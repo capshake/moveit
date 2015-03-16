8544359313932235b5bf5211c8f14a3be32a1360
79tobias <79tobias@googlemail.com>
2015-03-16 15:15:14 +0100

    Revert "removed user_name"
    This reverts commit 049aaf86f59094a23c88cb2adb7e96fec4ce3fb4.

--- pages/user/settings.php
+++ pages/user/settings.php
@@ -36,7 +36,12 @@ if (!$userData->isLoggedIn()) {
                     $db->bind("id", $userData->getUserId());
                     $user = $db->row("SELECT * FROM " . TABLE_USERS . " WHERE user_id = :id");
                     ?>
-					
+
+
+                    <div class="form-group">
+                        <label for="user_name">Benutzername</label>
+                        <input id="user_name" name="user_name" value="<?php echo $user['user_name']; ?>" class="first form-control" placeholder="Benutzername" type="text" required autofocus>
+                    </div>
                     <div class="form-group">
                         <label for="user_firstname">Vorname</label>
                         <input id="user_firstname" name="user_firstname" value="<?php echo $user['user_firstname']; ?>" class="first form-control" placeholder="Vorname" type="text" required autofocus>
@@ -47,7 +52,7 @@ if (!$userData->isLoggedIn()) {
                     </div>
                     <div class="form-group">
                         <label for="user_email">Email</label>
-                        <input id="user_email" name="user_email" value="<?php echo $user['user_email']; ?>" class="first form-control" placeholder="Email" type="email" required autofocus>
+                        <input id="user_email" name="user_email" value="<?php echo $user['user_email']; ?>" class="first form-control" placeholder="Email" type="text" required autofocus>
                     </div>
 
                     <button class="btn btn-primary btn-block" type="submit" name="edit">speichern</button>
