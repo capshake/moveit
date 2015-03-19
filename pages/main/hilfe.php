70ee4734e51cba2d5bd6c24d69f61aed3331009e
Isgru6359 <isabelle.gruchot@gmail.com>
2015-03-19 16:54:52 +0100

    Help accordeon edited

--- pages/main/hilfe.php
+++ pages/main/hilfe.php
@@ -22,7 +22,7 @@ if (!$userData->isLoggedIn()) {
         <div class=" col-md-8 col-md-offset-2">
             <div class="well">
                 <div id="akkordeon">
-                    <h3> +  Wie setzt sich die Seite zusammen? </h3>
+                    <h3>  Wie setzt sich die Seite zusammen? </h3>
                     <div>
                         <p>
                             Die Hauptseite des moveIT-Umzugplaners setzt sich aus den folgenden 3 Bereichen (von rechts nach links) zusammen:
@@ -40,7 +40,7 @@ if (!$userData->isLoggedIn()) {
                     </div>
                     <br>
                     <br>
-                    <h3>+  Welche Funktion hat der Altbaubereich? </h3>
+                    <h3> Welche Funktion hat der Altbaubereich? </h3>
                     <div>
                         <p>
                             Im Altbaubereich werden Ihnen genau zwei Funktionen angeboten:
@@ -59,11 +59,25 @@ if (!$userData->isLoggedIn()) {
                     </div>
                     <br>
                     <br>
-                    <h3>+  Was ist die Zollstock-Funktion? </h3>
+                    <h3> Welche Funktion hat der Neubaubereich? </h3>
 
 
                     <div>
-                        Die <b>Zollstock</b>-Funktion dient zur Abmessung von Abständen zwischen Möbelstücken. Dafür wählen Sie die Zollstock-Funktion aus, klicken dann einmal, um den Startpunkt festzulegen, und ein zweites mal, um den Endpunkt auszuwählen.
+                        <p>
+                            Im Neubaubereich findet die direkte Raumbearbeitung, d.h. Positionieren der Möbel, statt. Folgende Funktionen sind gegeben:
+                            <ul>
+                                <li><strong>Auswahl des Raumes:</strong> Über die Dropdown-Menüs kann ein Raum des Neubaus ausgewählt werden. 
+                                    Der ausgewählte Raum öffnet sich dann im mittleren Bereich und steht zur Bearbeitung zur Verfügung. </li>
+                                <li><strong>Der MAP-Button:</strong>Durch Klicken auf den MAP-Button öffnet sich ein Dialog, in dem der Grundriss
+                                    des Neubaus angezeigt wird. Das dient u.a. zur Ortung eines Raumes. Alternativ kann über die Karte ein Raum zur 
+                                    Bearbeitung ausgewählt werden - hierzu muss der entsprechende Raum angeklickt werden.
+                                </li>   
+                                <li><strong>Der Zollstock-Button:</strong> Durch Klicken auf den Zollstock-Button erscheinen im Raumbearbeitungsbereich zwei Winkel,
+                                    die innerhalb des Raumes positioniert werden können. Rechts von den Dropdown-Menüs wird Ihnen der Abstand zwischen den zwei Punkten
+                                    in cm angezeigt. 
+                                </li>
+                            </ul>
+                        </p>
                         <br>
                         <a href="#" data-toggle="modal" data-target="#lightbox">
                             <img class="anleitung" src="./img/3Anleitung.png">
@@ -71,9 +85,26 @@ if (!$userData->isLoggedIn()) {
                     </div>
                     <br>
                     <br>
-                    <h3>+  Wofür dienen die verschiedenen Lager? </h3>
+                    <h3>Welche Funktion haben die Lager?</h3>
                     <div>
-                        Falls Sie zunächst keine Verwendung für ein oder mehrere Möbelstücke finden, können Sie diese ebenfalls mittels drag-and-drop in das (persönliche) <b>Lager</b> verschieben. Möbelstücke, die defekt oder nicht mehr brauchbar sind, verschieben Sie in den <b>"Müll"</b>. Das <b>öffentliche Lager</b> dient der Möbelablage, die noch funktionstüchtig sind, für Sie aber keine weitere Verwendung haben.
+                        <p>
+                            <ul>
+                                <li><strong>Persönliches Lager:</strong>Falls Sie zunächst keine Verwendung für ein oder mehrere Möbelstücke finden,
+                                 können Sie diese ebenfalls mittels drag-and-drop in das (persönliche) Lager verschieben.</li>
+                                <li><strong>Müll:</strong>Möbelstücke, die defekt oder nicht mehr brauchbar sind, verschieben Sie ebenfalls per drag-and-drop in den "Müll".</li>
+                                <li><strong>Öffentliches Lager:</strong> Hier können Möbel abgelegt werden, die noch funktionstüchtig sind, für die Sie aber keine Verwendung mehr haben.</li>
+                            </ul>
+                        </p>
+                        <br>
+                        <a href="#" data-toggle="modal" data-target="#lightbox">
+                            <img class="anleitung" src="./img/3Anleitung.png">
+                        </a>
+                    </div>
+                    <br>
+                    <br>
+                    <h3> Wofür dienen die verschiedenen Lager? </h3>
+                    <div>
+                          
                         <a href="#" data-toggle="modal" data-target="#lightbox">
                             <img class="anleitung" src="./img/4Anleitung.png">
                         </a>
