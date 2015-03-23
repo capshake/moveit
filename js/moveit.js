36f3bb39661259472ff6cecf46320456651a60ed
Isgru6359 <isabelle.gruchot@gmail.com>
2015-03-23 21:21:29 +0100

    Dialogue size changed

--- js/moveit.js
+++ js/moveit.js
@@ -95,7 +95,7 @@ $("#mapEditorDialog").dialog({ //MapEditor Dialog
 
       $("#mapBearbeitenDialog").dialog({ //Map Bearbeiten Dialog
         autoOpen: false,
-        height: 400,
+        height: 500,
         width: 700,
         modal: true,
     });
