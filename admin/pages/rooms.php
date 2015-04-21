24479445fa81fc51996a467194dcb21fd627d4e7
Philip Stapelfeldt <phil@capshake.com>
2015-04-21 23:49:53 +0200

    - room map update

--- admin/pages/rooms.php
+++ admin/pages/rooms.php
@@ -16,36 +16,38 @@ if ($userData->isLoggedIn() && $userData->isAdmin()) {
         if (isset($_GET['remove'])) {
             $roomNameBeforeDelete = $db->row("SELECT room_name FROM " . TABLE_ROOMS . " WHERE room_id = :room_id", array("room_id" => $_GET['remove']));
 
-            if($db -> single("SELECT COUNT(*) FROM " . TABLE_ROOMS . ", " . TABLE_ITEMS . " WHERE item_room_id = room_id AND (room_id = :room_id OR item_origin_room = :room_id_origin)", array("room_id" => $_GET['remove'], "room_id_origin" => $_GET['remove'])) > 0){
-                if($db -> single("SELECT COUNT(*) FROM " . TABLE_ROOMS . ", " . TABLE_ITEMS . " WHERE item_room_id = room_id AND room_id = :room_id", array("room_id" => $_GET['remove'])) > 0){
+            if ($db->single("SELECT COUNT(*) FROM " . TABLE_ROOMS . ", " . TABLE_ITEMS . " WHERE item_room_id = room_id AND (room_id = :room_id OR item_origin_room = :room_id_origin)", array("room_id" => $_GET['remove'], "room_id_origin" => $_GET['remove'])) > 0) {
+                if ($db->single("SELECT COUNT(*) FROM " . TABLE_ROOMS . ", " . TABLE_ITEMS . " WHERE item_room_id = room_id AND room_id = :room_id", array("room_id" => $_GET['remove'])) > 0) {
                     $numberOfSuccessfullyDeletedRooms = 0;
                     $roomNotEmpty = 1;
-                }else{
-                    $numberOfSuccessfullyDeletedRooms = $db -> query("UPDATE " . TABLE_ROOMS . " SET room_type = 0 WHERE room_id = :room_id", array("room_id" => $_GET['remove']));
+                } else {
+                    $numberOfSuccessfullyDeletedRooms = $db->query("UPDATE " . TABLE_ROOMS . " SET room_type = 0 WHERE room_id = :room_id", array("room_id" => $_GET['remove']));
                 }
-            }else{
+            } else {
                 $numberOfSuccessfullyDeletedRooms = $db->query("DELETE FROM " . TABLE_ROOMS . " WHERE room_id = :room_id", array("room_id" => $_GET['remove']), PDO::FETCH_NUM);
             }
 
-			if ($numberOfSuccessfullyDeletedRooms == 1) { ?>
-				<div class="row">
-					<div class="col-md-12">
-						<div class="alert alert-success">Der Raum <em><?php echo $roomNameBeforeDelete['room_name'] ?></em> wurde gelöscht.</div>
-					</div>
-				</div>
-			<?php } else if($roomNotEmpty){ ?>
+            if ($numberOfSuccessfullyDeletedRooms == 1) {
+                ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-success">Der Raum <em><?php echo $roomNameBeforeDelete['room_name'] ?></em> wurde gelöscht.</div>
+                    </div>
+                </div>
+        <?php } else if ($roomNotEmpty) { ?>
                 <div class="row">
                     <div class="col-md-12">
                         <div class="alert alert-danger">Der Raum <em><?php echo $roomNameBeforeDelete['room_name'] ?></em> wurde <b>NICHT</b> gelöscht, da er noch Möbelstücke enthält.</div>
                     </div>
                 </div>
-            <?php }else { ?>
-				<div class="row">
-					<div class="col-md-12">
-						<div class="alert alert-danger">Der Löschvorgang war nicht erfolgreich.</div>
-					</div>
-				</div>
-			<?php }
+        <?php } else { ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-danger">Der Löschvorgang war nicht erfolgreich.</div>
+                    </div>
+                </div>
+            <?php
+            }
         }
         if (isset($_GET['edit'])) {
             $existsRoom = $db->row("SELECT room_id FROM " . TABLE_ROOMS . " WHERE room_id = :room_id", array("room_id" => $_GET['edit']), PDO::FETCH_NUM);
@@ -56,7 +58,7 @@ if ($userData->isLoggedIn() && $userData->isAdmin()) {
                     <div class="well">
                         <?php if (!$existsRoom) { ?>
                             <div class="alert alert-info">Der Raum existiert nicht!</div>
-                        <?php } else { ?>
+                            <?php } else { ?>
                             <form method="POST" action="<?php echo BASEDIR; ?>admin/rooms/edit/<?php echo $_GET['edit']; ?>" role="form">
 
                                 <?php
@@ -88,32 +90,32 @@ if ($userData->isLoggedIn() && $userData->isAdmin()) {
                                     <input id="room_name_alt" name="room_name_alt" value="<?php echo $room['room_name_alt'] ?>" class="form-control" placeholder="Alternativer Raumname (z.B. PPD-Nr)" type="text" autofocus>
                                 </div>
 
-                                <?php /*<div class="form-group">
-                                    <label for="room_map_id">Map</label>
-                                    <select id="room_map_id" class="form-control" name="room_map_id">
-                                        <?php
-                                        $maps = $db->query("SELECT map_id, map_building_id, map_floor, map_picture, map_scale_cm, map_scale_px, building_name FROM " . TABLE_MAPS . " LEFT JOIN " . TABLE_BUILDINGS . " ON map_building_id = building_id ORDER BY map_id");
-                                        foreach ($maps as $map) {
-                                            ?>
-                                            <option value="<?php echo $map['map_id']; ?>"<?php echo ($map['map_id'] == $room['room_map_id']) ? ' selected="selected"' : ''; ?>><?php echo $map['building_name']; ?> <?php echo getFloor($map['map_floor']); ?></option>
-                                            <?php
-                                        }
-                                        ?>
-                                    </select>
-                                </div>* ?>
-                                <?php /*<div class="form-group">
-                                    <label for="room_type">Wo befindet sich der Raum?</label>
-                                    <select id="room_type" class="form-control" name="room_type">
-                                        <option value="1" <?php echo ($room['room_type'] == 1) ? 'selected' : ''; ?>>Altbau</option>
-                                        <option value="2" <?php echo ($room['room_type'] == 2) ? 'selected' : ''; ?>>Neubau</option>
-                                    </select>
-                                </div> */ ?>
+                                <?php /* <div class="form-group">
+                                  <label for="room_map_id">Map</label>
+                                  <select id="room_map_id" class="form-control" name="room_map_id">
+                                  <?php
+                                  $maps = $db->query("SELECT map_id, map_building_id, map_floor, map_picture, map_scale_cm, map_scale_px, building_name FROM " . TABLE_MAPS . " LEFT JOIN " . TABLE_BUILDINGS . " ON map_building_id = building_id ORDER BY map_id");
+                                  foreach ($maps as $map) {
+                                  ?>
+                                  <option value="<?php echo $map['map_id']; ?>"<?php echo ($map['map_id'] == $room['room_map_id']) ? ' selected="selected"' : ''; ?>><?php echo $map['building_name']; ?> <?php echo getFloor($map['map_floor']); ?></option>
+                                  <?php
+                                  }
+                                  ?>
+                                  </select>
+                                  </div>* ?>
+                                  <?php /*<div class="form-group">
+                                  <label for="room_type">Wo befindet sich der Raum?</label>
+                                  <select id="room_type" class="form-control" name="room_type">
+                                  <option value="1" <?php echo ($room['room_type'] == 1) ? 'selected' : ''; ?>>Altbau</option>
+                                  <option value="2" <?php echo ($room['room_type'] == 2) ? 'selected' : ''; ?>>Neubau</option>
+                                  </select>
+                                  </div> */ ?>
 
 
                                 <button class="btn btn-primary" type="submit" name="edit">Speichern</button>
                             </form>
 
-                        <?php } ?>
+        <?php } ?>
                         <br />
 
                         <a href="<?php echo BASEDIR; ?>admin/rooms" class="btn btn-default">
@@ -141,7 +143,7 @@ if ($userData->isLoggedIn() && $userData->isAdmin()) {
                             if (isset($_POST['create'])) {
                                 $room_name = $_POST['room_name'];
                                 $room_map_id = $_POST['room_map_id'];
-                                $room_type = $db -> single("SELECT room_type FROM " . TABLE_ROOMS . ", " . TABLE_MAPS . " WHERE room_map_id = map_id AND map_id = :map_id", array("map_id" => $room_map_id));
+                                $room_type = $db->single("SELECT room_type FROM " . TABLE_ROOMS . ", " . TABLE_MAPS . " WHERE room_map_id = map_id AND map_id = :map_id", array("map_id" => $room_map_id));
 
                                 $update = json_decode($roomData->createRoom($_POST));
                                 if ($update->status == 'error') {
@@ -153,8 +155,7 @@ if ($userData->isLoggedIn() && $userData->isAdmin()) {
                                     ?>
                                     <div class="alert alert-success"><?php echo $update->msg . '<br>Bitte denken Sie daran, den Raum in der <a href="' . BASEDIR . 'admin/mapEditor/edit/' . $room_map_id . '">Karte der Etage</a> zu markieren. Erst dann lassen sich Möbel im Raum platzieren!'; ?></div>
                                     <?php
-                                }
-                                else if ($update->status == 'success') {
+                                } else if ($update->status == 'success') {
                                     ?>
                                     <div class="alert alert-success"><?php echo $update->msg; ?></div>
                                     <?php
@@ -175,11 +176,13 @@ if ($userData->isLoggedIn() && $userData->isAdmin()) {
                                 <label for="room_map_id">Map</label>
                                 <select id="room_map_id" class="form-control" name="room_map_id">
                                     <?php
-                                    $maps = $db->query("SELECT map_id, map_building_id, map_floor, map_picture, map_scale_cm, map_scale_px, building_name FROM " . TABLE_MAPS . " LEFT JOIN " . TABLE_BUILDINGS . " ON map_building_id = building_id ORDER BY map_id");
+                                    $maps = $db->query("SELECT map_id, map_building_id, map_floor, map_picture, map_scale_cm, map_scale_px, building_name, building_type FROM " . TABLE_MAPS . " LEFT JOIN " . TABLE_BUILDINGS . " ON map_building_id = building_id ORDER BY map_id");
                                     foreach ($maps as $map) {
-                                        ?>
-                                        <option value="<?php echo $map['map_id']; ?>"><?php echo $map['building_name']; ?> <?php echo getFloor($map['map_floor']); ?></option>
-                                        <?php
+                                        if ($map['building_type'] == 2) {
+                                            ?>
+                                            <option value="<?php echo $map['map_id']; ?>"><?php echo $map['building_name']; ?> <?php echo getFloor($map['map_floor']); ?></option>
+                                            <?php
+                                        }
                                     }
                                     ?>
                                 </select>
